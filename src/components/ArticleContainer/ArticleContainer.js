7eeeb7add53a43fab08be1738c7a44c6648de199
Alex Platt <alja.platt@gmail.com>
2022-01-29 11:41:55 +0000

    plan complete

---
+++ src/components/ArticleContainer/ArticleContainer.js
@@ -0,0 +1,31 @@
+import React from "react";
+import articles from "../../libs/articles";
+
+function ArticleContainer() {
+  return (
+    <div>
+      {articles.map((article, i) => {
+        return (
+          <div key={i}>
+            <ArticleContent
+              key={article.id}
+              title={article.title}
+              paragraphs={article.paragraphs}
+            />
+            {article.comments.map((comment) => {
+              return (
+                <CommentSection
+                  key={comment.id}
+                  name={comment.name}
+                  text={comment.text}
+                />
+              );
+            })}
+          </div>
+        );
+      })}
+    </div>
+  );
+}
+
+export default ArticleContainer;
